cf4f7f545bf40c450a40bf89f49be5ea882015fb
KotoriK <52659125+KotoriK@users.noreply.github.com>
2020-12-01 16:25:11 +0800

    update

--- api/r.go
+++ api/r.go
@@ -8,28 +8,28 @@ import (
 )
 
 type Resp struct {
-	head    http.Header
-	body    string
-	cookie  []*http.Cookie
-	context context.Context
-	host    string
-	method  string
-	url     string
-	length  int64
+	Head   http.Header `json:"head"`
+	Body   string
+	Cookie []*http.Cookie
+	Ctx    context.Context
+	Host   string
+	Method string
+	Url    string
+	Length int64
 }
 
 func Handler(writter http.ResponseWriter, req *http.Request) {
 	length := req.ContentLength
 	body, _ := ioutil.ReadAll(req.Body)
 	res := Resp{
-		head:    req.Header,
-		body:    string(body),
-		cookie:  req.Cookies(),
-		context: req.Context(),
-		host:    req.Host,
-		method:  req.Method,
-		url:     req.URL.RawPath,
-		length:  length,
+		Head:   req.Header,
+		Body:   string(body),
+		Cookie: req.Cookies(),
+		Ctx:    req.Context(),
+		Host:   req.Host,
+		Method: req.Method,
+		Url:    req.URL.RawPath,
+		Length: length,
 	}
 	json.NewEncoder(writter).Encode(res)
 }
