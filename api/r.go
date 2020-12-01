abbbb43beec06cc6733a486bca5beb6fa2b67f44
KotoriK <52659125+KotoriK@users.noreply.github.com>
2020-12-01 16:17:39 +0800

    update

--- api/r.go
+++ api/r.go
@@ -1,10 +1,35 @@
 package reflect
 
 import (
+	"context"
 	"encoding/json"
+	"io/ioutil"
 	"net/http"
 )
 
+type Resp struct {
+	head    http.Header
+	body    string
+	cookie  []*http.Cookie
+	context context.Context
+	host    string
+	method  string
+	url     string
+	length  int64
+}
+
 func Handler(writter http.ResponseWriter, req *http.Request) {
-	json.NewEncoder(writter).Encode(req)
+	length := req.ContentLength
+	body, _ := ioutil.ReadAll(req.Body)
+	res := Resp{
+		head:    req.Header,
+		body:    string(body),
+		cookie:  req.Cookies(),
+		context: req.Context(),
+		host:    req.Host,
+		method:  req.Method,
+		url:     req.URL.RawPath,
+		length:  length,
+	}
+	json.NewEncoder(writter).Encode(res)
 }
