ae676036e93e6208270b4680b2f3efb492b94863
KotoriK <52659125+KotoriK@users.noreply.github.com>
2020-12-15 23:06:33 +0800

    change position

---
+++ api/reflect/index.go
@@ -0,0 +1,44 @@
+package reflect
+
+import (
+	"context"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+)
+
+//Resp Response
+type Resp struct {
+	Head    http.Header
+	Trailer http.Header
+	Host    string
+	Method  string
+	URL     url.URL
+	Query   url.Values
+	Length  int64
+	Proto   string
+	Body    string
+	Cookie  []*http.Cookie
+	Ctx     context.Context
+}
+
+//ReflectHandler Handler
+func ReflectHandler(writter http.ResponseWriter, req *http.Request) {
+	length := req.ContentLength
+	body, _ := ioutil.ReadAll(req.Body)
+	res := Resp{
+		Head:    req.Header,
+		Body:    string(body),
+		Cookie:  req.Cookies(),
+		Ctx:     req.Context(),
+		Host:    req.Host,
+		Method:  req.Method,
+		URL:     *req.URL,
+		Query:   req.URL.Query(),
+		Length:  length,
+		Proto:   req.Proto,
+		Trailer: req.Trailer,
+	}
+	json.NewEncoder(writter).Encode(res)
+}
