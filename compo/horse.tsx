6dc2877046da333ab6629aacd4b598549bf5d14f
KotoriK <52659125+KotoriK@users.noreply.github.com>
2020-12-15 23:48:56 +0800

    feat:🐴

---
+++ compo/horse.tsx
@@ -0,0 +1,42 @@
+//🐴🐴🐴🐴🐴🐴🐴🐴🐴🐴=10
+import { useCallback, useEffect, useRef } from "react";
+const HORSE = '🐴'
+const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
+export default function useHorse() {
+    const _title = useRef<string>()
+    const handler = useCallback(() => {
+        let isHidden = document.visibilityState == "hidden"
+        if (isHidden) {
+            sleep(1500).then(() => {
+                document.title = '.'
+            })
+            .then(async()=>{
+                let count = 0
+                const addHorse = async() => {
+                    if (document.visibilityState == "hidden" ) {
+                        if(count<10){
+                           document.title += HORSE
+                        count++
+                        await sleep(1000)
+                        addHorse()  
+                        }
+                    } else {
+                        document.title = _title.current
+                    }
+                }
+                sleep(1000).then(addHorse)
+            }) 
+        }else{
+            document.title = _title.current
+        }
+    }, [_title.current])
+    useEffect(() => {
+        _title.current = document.title
+    }, [])
+    useEffect(() => {
+        document.addEventListener("visibilitychange", handler)
+        return () => {
+            document.removeEventListener("visibilitychange", handler)
+        }
+    }, [handler])
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,26 +1,31 @@
-import { Container, makeStyles, Paper ,CssBaseline} from "@material-ui/core";
+import { Container, makeStyles, Paper, CssBaseline } from "@material-ui/core";
+import Head from "next/head";
+import useHorse from "../compo/horse";
+
 import '../node_modules/github-markdown-css/github-markdown.css'
-const useStyles = makeStyles(theme=>{
+const useStyles = makeStyles(theme => {
     return {
-        gap:{
-            height:theme.spacing(5)
+        gap: {
+            height: theme.spacing(5)
         },
-       
     }
 })
 export default function App({ Component, pageProps }) {
     const styles = useStyles()
+    useHorse()
     return <>
-          <CssBaseline />
-
-    <Container>
-        <Paper>
-            <div className={styles.gap}></div>
-            <Container className="markdown-body">
-                <Component {...pageProps} />
-            </Container>
-            <div className={styles.gap}></div>
-        </Paper>
-    </Container></>
+        <CssBaseline />
+        <Head>
+            <title>kotorik/utils</title>
+        </Head>
+        <Container>
+            <Paper>
+                <div className={styles.gap}></div>
+                <Container className="markdown-body">
+                    <Component {...pageProps} />
+                </Container>
+                <div className={styles.gap}></div>
+            </Paper>
+        </Container></>
 
 }
