d4c59b62267cb872e9167ffc0e4977501a5fa304
KotoriK <52659125+KotoriK@users.noreply.github.com>
2021-04-17 20:32:10 +0800

    Update index.ts

--- api/callme/index.ts
+++ api/callme/index.ts
@@ -1,30 +1,31 @@
 import { NowRequest, NowResponse } from '@vercel/node'
 import { APIResult } from '../../src/api'
-import {performance} from 'perf_hooks'
+import { performance } from 'perf_hooks'
 export default async function (req: NowRequest, res: NowResponse) {
     const { url } = req.query
     let isOK = false
     let data
 
     if (typeof url == 'string') {
-        try {
+        console.log(url)
             const timeBeforeReq = performance.now()
-            const resp = await fetch(url)
-            const timeAfterReq = performance.now()
-            const {status,statusText,headers,redirected,type} = resp
-            const trailer = await resp.trailer
-            data = {
-                status,
-                statusText,
-                headers,
-                redirected,
-                type,
-                trailer,
-                time:timeAfterReq-timeBeforeReq
-            }
-        } catch (e) {
-            data = {type:'error',error:e}
-        }
+            await fetch(url).then(async resp => {
+                const timeAfterReq = performance.now()
+                const { status, statusText, headers, redirected, type } = resp
+                const trailer = await resp.trailer
+                data = {
+                    status,
+                    statusText,
+                    headers,
+                    redirected,
+                    type,
+                    trailer,
+                    time: timeAfterReq - timeBeforeReq
+                }
+            })
+                .catch(e => {
+                    data = { type: 'error', error: e }
+                })
     } else {
         data = `malform url. Expect string, but get ${typeof url}`
     }
