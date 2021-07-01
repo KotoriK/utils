34ce1e00044cfb67ac0bd2b8abd5309e39bb8fd5
KotoriK <52659125+KotoriK@users.noreply.github.com>
2021-07-01 17:10:45 +0800

    fix(qqmusic.cover): error handling

--- api/qqmusic/cover.ts
+++ api/qqmusic/cover.ts
@@ -3,28 +3,37 @@ import { VercelResponse, VercelRequest } from '@vercel/node'
 import Cheerio from 'cheerio'
 import fetch from 'node-fetch'
 export default async function (req: VercelRequest, res: VercelResponse) {
-    const query = req.query;
-    const url: string = query.url as any
-    const search = query.search ? true : false
     let ok = false
     let data = ''
-    const resp = await fetch(url)
-    if (resp.ok) {
-        const $ = Cheerio.load(await resp.text())
-        const listName = $("#p_name_show").text()
-        const listAuthor = $(".data__cont .data__singer > a").text()
-        const coverImg = $('.data__cover > .data__photo').attr().src
-        if(search){
+    const query = req.query;
+    const url: string = query.url as any
+    if (url) {
+        try {
+            const search = query.search ? true : false
+            const resp = await fetch(url)
+            if (resp.ok) {
+                const $ = Cheerio.load(await resp.text())
+                const listName = $("#p_name_show").text()
+                const listAuthor = $(".data__cont .data__singer > a").text()
+                const coverImg = $('.data__cover > .data__photo').attr().src
+                if (search) {
 
-        }else{
-            data = JSON.stringify({
-                listName,
-                listAuthor,
-                coverImg
-            })
+                } else {
+                    ok = true
+                    data = JSON.stringify({
+                        listName,
+                        listAuthor,
+                        coverImg
+                    })
+                }
+            } else {
+                data = 'fetch failed:' + resp.status
+            }
+        } catch (error) {
+            data = JSON.stringify(error)
         }
     } else {
-        data = 'fetch failed:' + resp.status
+        data = 'argument miss: "url"'
     }
     res.send(`{ok:${ok ? 'true' : 'false'},data:${data}}`);
 }
