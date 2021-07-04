5b99f83e7e58b5bac404dc8b0efab424db9dc2e9
KotoriK <52659125+KotoriK@users.noreply.github.com>
2021-07-04 15:11:28 +0800

    update transition

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,15 +3,17 @@ import Head from "next/head";
 import useHorse from "../compo/horse";
 import { PageTransition } from 'next-page-transitions'
 import '../page-transition.css'
+import { useRouter } from "next/dist/client/router";
 export default function App({ Component, pageProps }) {
     useHorse()
+    const router = useRouter()
     return <>
         <CssBaseline />
         <Head>
             <title>kotorik/utils</title>
         </Head>
         <PageTransition timeout={300} classNames="page-transition">
-            <Component {...pageProps} />
+            <Component {...pageProps} key={router.route}/>
         </PageTransition>
     </>
 
